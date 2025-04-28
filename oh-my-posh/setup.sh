e3a485dc4949aae94f0a9abe4183c3cae7ec0e57
Dominik Kruczek <TheKiromen@gmail.com>
2025-04-28 18:14:17 +0200

    Add initial programs to setup script

--- oh-my-posh/setup.sh
+++ oh-my-posh/setup.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-echo $0
+#Install packages
+choco install -y oh-my-posh
+choco install -y terraform
+choco install -y kubens kubectx
+
+
+# Run oh-my-posh setup
+
+# Pull bash configs and themes
+
+# Save configs on local machine
