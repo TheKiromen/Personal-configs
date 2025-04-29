155cf386c10776f85a4f59dd80d2334fd7f27006
Dominik Kruczek <TheKiromen@gmail.com>
2025-04-29 17:29:21 +0200

    Add font installation for omp

--- oh-my-posh/setup.sh
+++ oh-my-posh/setup.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
+
 #Install packages
-choco install -y oh-my-posh
 choco install -y terraform
 choco install -y kubens kubectx
 
 # Setup OMP
 echo "Setting up OMP..."
+choco install -y oh-my-posh
 
-# Setup OMP plugins
-echo "Setting up OMP plugins..."
+echo "Installing JetBrains Mono font..."
+FONT_DIR = "$HOME/.local/share/fonts/JetBrainsMono"
+mkdir -p $FONT_DIR
+cd $FONT_DIR
+curl -L -o "JetBrainsMono.zip" "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/JetBrainsMono.zip"
+unzip JetBrainsMono.zip
+rm JetBrainsMono.zip
+fc-cache -f -v # Refresh cache just in case
 
 # Setup .bashrc and themes
 echo "Setting up .bashrc..."
+
+# Setup OMP plugins
+echo "Setting up OMP plugins..."
