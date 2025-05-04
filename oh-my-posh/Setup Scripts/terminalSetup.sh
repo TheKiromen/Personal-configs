afeecc90f5cdd2cd6e166de54b833f7c19c760af
Dominik Kruczek <TheKiromen@gmail.com>
2025-05-04 16:29:12 +0200

    Fix uuid generation and config formatting

--- oh-my-posh/Setup Scripts/terminalSetup.sh
+++ oh-my-posh/Setup Scripts/terminalSetup.sh
@@ -26,12 +26,12 @@ rm tmp -r
 
 # Create profile for Git Bash
 WT_CONFIG="$LOCALAPPDATA/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json"
-CONFIG_UUID=$(uuidgen)
+CONFIG_UUID=$(powershell -Command "[guid]::NewGuid().ToString()")
 # Check if file contains generated UUID
 MAX_ATTEMPTS=100
 ATTEMPT=0
 while grep -q "$CONFIG_UUID" "$WT_CONFIG"; do
-    CONFIG_UUID=$(uuidgen)
+    CONFIG_UUID=$(powershell -Command "[guid]::NewGuid().ToString()")
     ATTEMPT=$((ATTEMPT + 1))
     if [ $ATTEMPT -ge $MAX_ATTEMPTS ]; then
         echo "Error: Could not generate a unique UUID after $MAX_ATTEMPTS attempts."
@@ -39,19 +39,22 @@ while grep -q "$CONFIG_UUID" "$WT_CONFIG"; do
     fi
 done
 # Prepare the config block
-CONFIG_BLOCK=",{
-    \"colorScheme\": \"Dark+\",
-    \"commandline\": \"%PROGRAMFILES%\\\\Git\\\\usr\\\\bin\\\\bash.exe -i -l\",
-    \"font\": {
-        \"face\": \"$FONT_FACE_NAME\",
-        \"size\": 10
+CONFIG_BLOCK=$(cat <<EOF
+{
+    "colorScheme": "Dark+",
+    "commandline": "%PROGRAMFILES%\\\\Git\\\\usr\\\\bin\\\\bash.exe -i -l",
+    "font": {
+        "face": "$FONT_FACE_NAME",
+        "size": 10
     },
-    \"guid\": \"{$CONFIG_UUID}\",
-    \"hidden\": false,
-    \"icon\": \"C:\\\\Program Files\\\\Git\\\\mingw64\\\\share\\\\git\\\\git-for-windows.ico\",
-    \"name\": \"Git Bash\",
-    \"startingDirectory\": \"$HOME\"
-}"
+    "guid": "{$CONFIG_UUID}",
+    "hidden": false,
+    "icon": "C:\\\\Program Files\\\\Git\\\\mingw64\\\\share\\\\git\\\\git-for-windows.ico",
+    "name": "Git Bash",
+    "startingDirectory": "$HOME"
+}
+EOF
+)
 
 # Add new profile to the settings.json and set it as default
 
