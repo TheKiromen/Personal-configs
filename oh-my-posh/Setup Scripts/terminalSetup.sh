cb10edf4e974ca10c9611551814ba66423c63824
Dominik Kruczek <TheKiromen@gmail.com>
2025-05-04 16:38:41 +0200

    Add execution logs

--- oh-my-posh/Setup Scripts/terminalSetup.sh
+++ oh-my-posh/Setup Scripts/terminalSetup.sh
@@ -25,6 +25,7 @@ cd ".."
 rm tmp -r
 
 # Create profile for Git Bash
+echo "Creating profile for Git Bash..."
 WT_CONFIG="$LOCALAPPDATA/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json"
 CONFIG_UUID=$(powershell -Command "[guid]::NewGuid().ToString()")
 # Check if file contains generated UUID
@@ -57,13 +58,19 @@ EOF
 )
 
 # Add new profile to the settings.json and set it as default
+echo "Adding profile to settings.json..."
 jq --argjson bashProfile "$CONFIG_BLOCK" '.profiles.list += [$bashProfile]' "$WT_CONFIG" > temp.json && mv temp.json settings.json
 jq --arg newDefaultProfile "{$CONFIG_UUID}" '.defaultProfile = $newDefaultProfile' "$WT_CONFIG" > temp.json && mv temp.json settings.json
 
 # Update git bash settings just in case
+echo "Updating git bash settings..."
 Set the font in git bash config in case it is used outside of windows terminal
 if grep -q '^Font=' ~/.minttyrc; then
     sed -i '/^Font=/c\Font=JetBrainsMono NFM Medium' ~/.minttyrc
 else
     echo 'Font=JetBrainsMono NFM Medium' >> ~/.minttyrc
 fi
+
+# Launch Windows Terminal to verify the changes
+echo "Launching Windows Terminal..."
+wt
