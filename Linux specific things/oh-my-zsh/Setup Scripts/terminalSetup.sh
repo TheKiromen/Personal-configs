222b888bd5b12ad6cc0ce7155549bee7858bf693
Dominik Kruczek <TheKiromen@gmail.com>
2025-05-08 18:51:23 +0200

    Rename zsh script

--- Linux specific things/oh-my-zsh/Setup Scripts/terminalSetup.sh
+++ Linux specific things/oh-my-zsh/Setup Scripts/terminalSetup.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-
 # Setup ZSH
 echo "Setting up ZSH..."
-sudo apt install zsh
-
-# Setup ZSH plugins
-echo "Setting up ZSH plugins..."
+sudo apt install -y zsh
+# Make ZSH the default shell, restart needed for this to take effect
+chsh -s $(which zsh)
 
 # Setup .zshrc and themes
 echo "Setting up .zshrc..."
+
+# Setup ZSH plugins
+echo "Setting up ZSH plugins..."
 
 # Install packages
 echo "Installing packages..."
@@ -17,3 +18,6 @@ DIVE_VERSION=$(curl -sL "https://api.github.com/repos/wagoodman/dive/releases/la
 curl -fOL "https://github.com/wagoodman/dive/releases/download/v${DIVE_VERSION}/dive_${DIVE_VERSION}_linux_amd64.deb"
 sudo apt install ./dive_${DIVE_VERSION}_linux_amd64.deb
 sudo apt install git-all
+
+# Prompt user to restart the computer
+echo "ZSH setup complete. Please restart your computer for the changes to take effect."
