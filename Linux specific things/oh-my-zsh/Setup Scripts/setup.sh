84f5e771f81533c51a6a46c876d3ce4a64ed65f3
Dominik Kruczek <TheKiromen@gmail.com>
2025-05-08 18:21:40 +0200

    Move packages after zsh config

--- Linux specific things/oh-my-zsh/Setup Scripts/setup.sh
+++ Linux specific things/oh-my-zsh/Setup Scripts/setup.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Install packages
-echo "Installing packages..."
-DIVE_VERSION=$(curl -sL "https://api.github.com/repos/wagoodman/dive/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
-curl -fOL "https://github.com/wagoodman/dive/releases/download/v${DIVE_VERSION}/dive_${DIVE_VERSION}_linux_amd64.deb"
-sudo apt install ./dive_${DIVE_VERSION}_linux_amd64.deb
-sudo apt install git-all
 
 # Setup ZSH
 echo "Setting up ZSH..."
@@ -17,3 +11,10 @@ echo "Setting up ZSH plugins..."
 
 # Setup .zshrc and themes
 echo "Setting up .zshrc..."
+
+# Install packages
+echo "Installing packages..."
+DIVE_VERSION=$(curl -sL "https://api.github.com/repos/wagoodman/dive/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
+curl -fOL "https://github.com/wagoodman/dive/releases/download/v${DIVE_VERSION}/dive_${DIVE_VERSION}_linux_amd64.deb"
+sudo apt install ./dive_${DIVE_VERSION}_linux_amd64.deb
+sudo apt install git-all
