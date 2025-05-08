49f0bd7565cb4b8d8a95b68c6fea3ba7fe2e2e7a
Dominik Kruczek <TheKiromen@gmail.com>
2025-05-08 19:16:21 +0200

    Install required packages beforehand

--- Linux specific things/oh-my-zsh/Setup Scripts/terminalSetup.sh
+++ Linux specific things/oh-my-zsh/Setup Scripts/terminalSetup.sh
@@ -5,7 +5,10 @@ echo "Setting up ZSH..."
 sudo apt install -y zsh
 # Make ZSH the default shell, restart needed for this to take effect
 chsh -s $(which zsh)
-exec $(which zsh)
+
+# Install required packages
+echo "Installing packages required for setup..."
+sudo apt install git-all
 
 # Setup .zshrc and themes
 echo "Setting up .zshrc..."
@@ -13,12 +16,11 @@ echo "Setting up .zshrc..."
 # Setup ZSH plugins
 echo "Setting up ZSH plugins..."
 
-# Install packages
-echo "Installing packages..."
+# Install supplementary packages
+echo "Installing supplementary packages..."
 DIVE_VERSION=$(curl -sL "https://api.github.com/repos/wagoodman/dive/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
 curl -fOL "https://github.com/wagoodman/dive/releases/download/v${DIVE_VERSION}/dive_${DIVE_VERSION}_linux_amd64.deb"
 sudo apt install ./dive_${DIVE_VERSION}_linux_amd64.deb
-sudo apt install git-all
 
 # Prompt user to restart the computer
 echo "ZSH setup complete. Please restart your computer for the changes to take effect."
