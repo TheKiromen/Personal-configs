965a0ae276453fe4a526fc7b49bea7429b1c5460
Dominik Kruczek <TheKiromen@gmail.com>
2025-05-25 13:24:53 +0200

    Add terminal packages

--- Linux specific things/oh-my-zsh/Setup Scripts/terminalSetup.sh
+++ Linux specific things/oh-my-zsh/Setup Scripts/terminalSetup.sh
@@ -52,8 +52,10 @@ sudo systemctl enable docker
 DIVE_VERSION=$(curl -sL "https://api.github.com/repos/wagoodman/dive/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
 curl -fOL "https://github.com/wagoodman/dive/releases/download/v${DIVE_VERSION}/dive_${DIVE_VERSION}_linux_amd64.deb"
 sudo apt install ./dive_${DIVE_VERSION}_linux_amd64.deb
-
-
+# Fzf
+sudo apt install -y fzf
+# Lsd
+sudo apt install -y lsd
 
 # Prompt user to restart the computer
 echo "ZSH setup complete. Please restart your computer for the changes to take effect."
